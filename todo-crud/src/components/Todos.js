0da93320f824c5deac01dcf6dd301c7a60eb4dd7
reenahukum <big.b.it.traininig@gmail.com>
2020-02-02 18:02:25 +0545

    Installing required packages

---
+++ todo-crud/src/components/Todos.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Todos extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Todos
