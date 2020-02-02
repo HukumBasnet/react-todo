1416e98ab86473830cdc28545be4d2f341b05513
reenahukum <big.b.it.traininig@gmail.com>
2020-02-02 19:27:54 +0545

    added crud operation

--- todo-crud/src/components/Todos.js
+++ todo-crud/src/components/Todos.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem';
 import PropTypes from 'prop-types'
 
 export class Todos extends Component {
-    static propTypes = {
-
-    }
 
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+            ));
+        
     }
+}
+
+//PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
 }
 
 export default Todos
